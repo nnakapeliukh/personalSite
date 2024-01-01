637d71719fc3e54c83e793f7d4f880fb4c552f71
Nazar <nnakapeliukh@gmail.com>
2023-12-31 18:06:17 -0600

    Moves text a bit higher

--- src/elements/mainPage.tsx
+++ src/elements/mainPage.tsx
@@ -1,53 +1,54 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import React from 'react';
-import mountainBgd from '../assets/mountainEnhanced.jpg'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import React from "react";
+import mountainBgd from "../assets/mountainEnhanced.jpg";
 
 function MainPage() {
   return (
-    <Container className='hero route bg-image min-vw-100'
+    <Container
+      className="hero route bg-image min-vw-100"
       style={{
         backgroundImage: `url(${mountainBgd})`,
-        height: '100vh',
-        width: '100%',
-        backgroundAttachment: 'fixed',
-        backgroundPosition: '50% 50%'
+        height: "100vh",
+        width: "100%",
+        backgroundAttachment: "fixed",
+        backgroundPosition: "50% 50%",
       }}
     >
       <Container>
-        <Row>        
-          <Col xs
-            style={{ height: '100vh' }}
-          >
+        <Row>
+          <Col xs style={{ height: "100vh" }}>
             <p
               style={{
                 // height: '40%',
-                paddingTop: '30%',
-                textShadow: '11px 11px 11px #000',
-                color: 'white',
-                fontFamily: 'arial, serif',
+                paddingTop: "20%",
+                textShadow: "11px 11px 11px #000",
+                color: "white",
+                fontFamily: "arial, serif",
               }}
-              className='display-2 fw-bold lh-lg text-center'>
+              className="display-2 fw-bold lh-lg text-center"
+            >
               Hi, I&apos;m Nazar
             </p>
             <p
               style={{
                 // paddingTop: '40%',
                 // height: '30%',
-                textShadow: '11px 11px 11px #000',
-                color: 'white',
-                fontFamily: 'arial, serif',
+                textShadow: "11px 11px 11px #000",
+                color: "white",
+                fontFamily: "arial, serif",
               }}
-              className='display-6 align-left lh-lg text-center'>
+              className="display-6 align-left lh-lg text-center"
+            >
               and I thrive on challenge
             </p>
           </Col>
           <Col xs={12} md>
-            {/* Placeholder */}           
+            {/* Placeholder */}
           </Col>
-        </Row>      
-      </Container>      
+        </Row>
+      </Container>
     </Container>
   );
 }
