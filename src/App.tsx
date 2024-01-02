cadd3fe61dbdcb87c438c5cb763f25887ced45dc
Nazar <nnakapeliukh@gmail.com>
2024-01-02 12:50:37 -0600

    Adds helmet to change tab name and icon

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import "./elements/highlights.css";
 // import lalptopImage from './assets/lap2.png';
 import NavigationBar from "./elements/navBar";
 import MainPage from "./elements/mainPage";
-// import { Image } from 'react-bootstrap';
 import AboutMe from "./elements/aboutMe";
 import Highlights from "./elements/highlights";
+import { Helmet } from "react-helmet";
+import nazarLogo from "./assets/nazarLogo.png";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Helmet>
+          <title> Nazar&apos;s Webpage</title>
+          <link rel="icon" type="image/png" href={nazarLogo} sizes="16x16" />
+        </Helmet>
         <NavigationBar />
         <MainPage />
         <AboutMe />
