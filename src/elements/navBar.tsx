942623b8aae925fc6708510c76e57c634f405f4e
Nazar <nnakapeliukh@gmail.com>
2024-01-03 16:02:16 -0600

    Adds bottom navigation

--- src/elements/navBar.tsx
+++ src/elements/navBar.tsx
@@ -2,7 +2,9 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
 import nazarLogo from "./../assets/nazarLogo.png";
-import { Nav, NavDropdown } from "react-bootstrap";
+import githubLogo from "./../assets/github-mark-white.svg";
+import linmkedInLogo from "./../assets/In-White-26.png";
+import { Image, Nav, NavDropdown } from "react-bootstrap";
 
 function NavigationBar() {
   return (
@@ -10,7 +12,7 @@ function NavigationBar() {
       <Navbar
         collapseOnSelect
         expand="lg"
-        className="bg-body-tertiary"
+        className="bg-dark"
         data-bs-theme="dark"
         sticky="top"
       >
@@ -35,6 +37,7 @@ function NavigationBar() {
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#about">About me</Nav.Link>
               <Nav.Link href="#highlights">Highlights</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
               <NavDropdown title="More" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#resume">
                   More to come...
@@ -51,6 +54,29 @@ function NavigationBar() {
             </Nav>
           </Navbar.Collapse>
         </Container>
+      </Navbar>
+
+      {/* Bottom navbar */}
+      <Navbar className="bg-dark p-0" data-bs-theme="dark" fixed="bottom">
+        <Container className="justify-content-center">
+          <Nav>
+            <Nav.Link
+              className="p-1 px-3"
+              href="https://github.com/nnakapeliukh/personalSite"
+            >
+              <Image src={githubLogo} height={"16px"} />
+            </Nav.Link>
+            <Nav.Link
+              className="p-1 px-3"
+              href="https://www.linkedin.com/in/nazar-nakapeliukh-813603114/"
+            >
+              <Image src={linmkedInLogo} height={"16px"} />
+            </Nav.Link>
+          </Nav>
+        </Container>
+        <Nav.Item className="justify-content-end mx-3 text-white">
+          <small>2024</small>
+        </Nav.Item>
       </Navbar>
     </>
   );
