24d166e626d7998372942f12b238ed52f7c2ff9e
Nazar <nnakapeliukh@gmail.com>
2024-01-03 16:01:54 -0600

    Adds cloud animation

--- src/elements/mainPage.tsx
+++ src/elements/mainPage.tsx
@@ -3,54 +3,114 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import React from "react";
 import mountainBgd from "../assets/mountainEnhanced.jpg";
+import cloudsBgd from "../assets/clouds.png";
+import cloudsBgd2 from "../assets/clouds2.png";
+import cloudsBgd3 from "../assets/clouds3.png";
+import "./mainPage.css";
+// import { Image } from "react-bootstrap";
 
 function MainPage() {
   return (
-    <Container
-      className="hero route bg-image min-vw-100"
-      style={{
-        backgroundImage: `url(${mountainBgd})`,
-        height: "100vh",
-        width: "100%",
-        backgroundAttachment: "fixed",
-        backgroundPosition: "50% 50%",
-      }}
-      id="home"
-    >
-      <Container>
-        <Row>
-          <Col xs style={{ height: "100vh" }}>
-            <p
+    <>
+      <Container
+        className="hero route bg-image min-vw-100 p-0"
+        style={{
+          backgroundImage: `url(${mountainBgd})`,
+          height: "100vh, 100vh",
+          width: "100%, 100%",
+          backgroundAttachment: "fixed",
+          backgroundPosition: "50% 50%",
+          backgroundRepeat: "no-repeat",
+        }}
+        id="home"
+      >
+        {/* Animated clouds */}
+        <div
+          className="hero route bg-image min-vw-100 p-0 m-0 animate1"
+          style={{
+            backgroundImage: `url(${cloudsBgd})`,
+            height: "100vh",
+            width: "100px",
+            backgroundAttachment: "fixed",
+            backgroundPosition: "50% 50%",
+            backgroundRepeat: "no-repeat",
+            position: "absolute",
+            zIndex: " 0",
+          }}
+        />
+        <div
+          className="hero route bg-image min-vw-100 p-0 m-0 animate1"
+          style={{
+            backgroundImage: `url(${cloudsBgd2})`,
+            height: "100vh",
+            width: "100px",
+            backgroundAttachment: "fixed",
+            backgroundPosition: "50% 50%",
+            backgroundRepeat: "no-repeat",
+            position: "absolute",
+            zIndex: " 0",
+          }}
+        />
+        <div
+          className="hero route bg-image min-vw-100 p-0 m-0 animate2"
+          style={{
+            backgroundImage: `url(${cloudsBgd3})`,
+            height: "100vh",
+            width: "100px",
+            backgroundAttachment: "fixed",
+            backgroundPosition: "50% 50%",
+            backgroundRepeat: "no-repeat",
+            position: "absolute",
+            zIndex: " 0",
+          }}
+        />
+
+        <Container>
+          <Row>
+            <Col
+              xs
               style={{
-                // height: '40%',
-                paddingTop: "20%",
-                textShadow: "11px 11px 11px #000",
-                color: "white",
-                fontFamily: "arial, serif",
+                height: "100vh",
+                paddingTop: "10%",
+                zIndex: "1",
               }}
-              className="display-2 fw-bold lh-lg text-center"
             >
-              Hi, I&apos;m Nazar
-            </p>
-            <p
-              style={{
-                // paddingTop: '40%',
-                // height: '30%',
-                textShadow: "11px 11px 11px #000",
-                color: "white",
-                fontFamily: "arial, serif",
-              }}
-              className="display-6 align-left lh-lg text-center"
-            >
-              and I thrive on challenge
-            </p>
-          </Col>
-          <Col xs={12} md>
-            {/* Placeholder */}
-          </Col>
-        </Row>
+              <Container
+                // className="bg-dark"
+                style={{
+                  borderRadius: "20px",
+                  fontFamily: "Monospace",
+                }}
+              >
+                <p
+                  style={{
+                    textShadow: "11px 11px 11px #000",
+                    color: "white",
+                  }}
+                  className="display-2 fw-bold lh-lg text-center  bg-dark py-0"
+                >
+                  Hi, I&apos;m Nazar
+                </p>
+                <p
+                  style={{
+                    // paddingTop: '40%',
+                    // height: '30%',
+                    textShadow: "11px 11px 11px #000",
+                    color: "white",
+                  }}
+                  className="display-6 align-left lh-lg text-center bg-dark"
+                >
+                  and I thrive on challenge
+                </p>
+              </Container>
+            </Col>
+            <Col xs={12} md>
+              {/* Placeholder */}
+            </Col>
+          </Row>
+        </Container>
       </Container>
-    </Container>
+    </>
   );
 }
 
