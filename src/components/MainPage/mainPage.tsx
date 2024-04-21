6713f9a5c3b1eb98d9c477fb7dff86f153d5a793
Nazar <nnakapeliukh@gmail.com>
2024-04-21 18:46:12 -0500

    Rework main page for react router

--- src/components/MainPage/mainPage.tsx
+++ src/components/MainPage/mainPage.tsx
@@ -2,23 +2,15 @@ import React from "react-dom";
 import AboutMe from "../aboutMe";
 import Highlights from "../highlights";
 import Projects from "../projects";
-import { Helmet } from "react-helmet";
-import nazarLogo from "../../assets/nazarLogo.png";
 import HeroContent from "../heroContent";
 function MainPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Helmet>
-          <title> Nazar&apos;s Webpage</title>
-          <link rel="icon" type="image/png" href={nazarLogo} sizes="16x16" />
-        </Helmet>
-        <HeroContent />
-        <AboutMe />
-        <Highlights />
-        <Projects />
-      </header>
-    </div>
+    <>
+      <HeroContent />
+      <AboutMe />
+      <Highlights />
+      <Projects />
+    </>
   );
 }
 
