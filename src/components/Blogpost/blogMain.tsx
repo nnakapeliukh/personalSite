f925e22b09573671a46da7b2190771309641f7b3
Nazar <nnakapeliukh@gmail.com>
2024-05-29 22:30:00 -0500

    Save progress

--- src/components/Blogpost/blogMain.tsx
+++ src/components/Blogpost/blogMain.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
 // // import axios, { AxiosError } from "axios";
@@ -7,15 +7,29 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 
 import { useSelector } from "react-redux";
 import { selectUserData } from "../../features/users/userSlice";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function BlogMain() {
+  const posts = useRef([]);
+  useEffect(() => {
+    console.log("trying to get posts");
+    axios
+      .get("http://localhost:3060/api/blog")
+      .then((res) => {
+        posts.current = res.data;
+    console.log(posts.current);
+
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
   // get user data from redux
   const userData = useSelector(selectUserData);
   console.log(userData);
   // validation schema
-
+  const navigate = useNavigate();
   return (
     <Container fluid="sm" className="shadow my-5 p-4 s">
       <Row className="justify-content-center">
@@ -24,22 +38,24 @@ export default function BlogMain() {
           <Link className="btn btn-info my-4" to={"create"}>
             Create New Post{" "}
           </Link>
-          <Button onClick={async ()=>{
-            const resp = await axios.post(
-              "http://localhost:3060/api/blog/create"              
-            );
-            console.log(resp);
-          }}>Create generic post</Button>
-          
-          <Button onClick={async ()=>{
-            const resp = await axios.get(
-              "http://localhost:3060/api/blog"              
-            );
-            console.log(resp);
-          }}>Get</Button>
+          <Button
+            onClick={async () => {
+              try {
+                const resp = await axios.post(
+                  "http://localhost:3060/api/blog/create"
+                );
+                console.log(resp);
+              } catch (e) {
+                navigate("/signin");
+              }
+            }}
+          >
+            Create generic post
+          </Button>
+
           <p className="alert alert-light">
             The purpose of the blog is to have a reminder and a quick reference
-            on some of the things that I work on.
+            on some of the things that I worked on.
           </p>
         </Col>
       </Row>
