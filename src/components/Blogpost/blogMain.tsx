0a5effe71d05c775fda59e030544ad641f5dfbf9
Nazar <nnakapeliukh@gmail.com>
2024-05-30 22:31:23 -0500

    Start displaying posts' short version on the main blog page

--- src/components/Blogpost/blogMain.tsx
+++ src/components/Blogpost/blogMain.tsx
@@ -5,29 +5,36 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 // import * as formik from "formik";
 // import * as yup from "yup";
 
-import { useSelector } from "react-redux";
-import { selectUserData } from "../../features/users/userSlice";
+// import { useSelector } from "react-redux";
+// import { selectUserData } from "../../features/users/userSlice";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
+interface postPreview {
+  title: string;
+  description: string;
+  post_date: Date;
+  tags: Array<string>;
+  user_name: string;
+}
+
 export default function BlogMain() {
-  const posts = useRef([]);
+  const posts = useRef<Array<postPreview> | undefined>(undefined);
+
   useEffect(() => {
-    console.log("trying to get posts");
     axios
       .get("http://localhost:3060/api/blog")
       .then((res) => {
         posts.current = res.data;
-    console.log(posts.current);
-
+        console.log("response for get blog posts", posts.current);
       })
       .catch((e) => {
         console.log(e);
       });
   }, []);
   // get user data from redux
-  const userData = useSelector(selectUserData);
-  console.log(userData);
+  // const userData = useSelector(selectUserData);
+  // console.log("user data from redux",userData);
   // validation schema
   const navigate = useNavigate();
   return (
@@ -44,7 +51,7 @@ export default function BlogMain() {
                 const resp = await axios.post(
                   "http://localhost:3060/api/blog/create"
                 );
-                console.log(resp);
+                console.log("response for 'post' post", resp);
               } catch (e) {
                 navigate("/signin");
               }
@@ -57,6 +64,14 @@ export default function BlogMain() {
             The purpose of the blog is to have a reminder and a quick reference
             on some of the things that I worked on.
           </p>
+          {posts.current?.map((post, index) => {
+            return (
+              <Container key={"postContainer" + index} className="bg-light rounded ">
+                <h3 key={post.title+index}>{post.title}</h3>
+                <p key={"postDesckey" + index}>{post.description}</p>
+              </Container>
+            );
+          })}
         </Col>
       </Row>
     </Container>
