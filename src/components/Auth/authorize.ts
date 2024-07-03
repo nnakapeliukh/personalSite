1e3753e314cdf9e50595ca56c05fb9366577b28d
Nazar <nnakapeliukh@gmail.com>
2024-07-03 14:26:28 -0500

    Add token expiry handling

--- src/components/Auth/authorize.ts
+++ src/components/Auth/authorize.ts
@@ -5,27 +5,24 @@ import { setUserData } from "../../features/users/userSlice";
 export function Authorize() {
   const dispatch = useDispatch();
   const token = localStorage.token;
-  if (token){
-    axios.defaults.headers.common["Authorization"] =
-    `Bearer ${token}`;
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-
-  axios
-    .get("http://localhost:3060/api/users/")
-    .then((response) => {
-      dispatch(
-        setUserData({
-          userName: response.data.userName,
-          firstName: response.data.firstName,
-          lastName: response.data.lastName,
-          city: response.data.city,
-        })
-      );
-    })
-    .catch(() => {
-      // silent catch on auth fail
-    }
-    );
+    axios
+      .get("http://localhost:3060/api/users/")
+      .then((response) => {
+        dispatch(
+          setUserData({
+            userName: response.data.userName,
+            firstName: response.data.firstName,
+            lastName: response.data.lastName,
+            city: response.data.city,
+          })
+        );
+      })
+      .catch(() => {
+        //remove token in case of expiry
+        localStorage.removeItem("token");
+      });
   }
- 
 }
